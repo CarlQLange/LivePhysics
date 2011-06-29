663ff96b4190407f0b1ef0049d48905c4e998317
Carl Lange <carl@flax.ie>
2011-06-29 16:31:05 +0100

    Added tracking of milliseconds of execution.

--- LivePhysics/src/ie/flax/livephysics/client/LivePhysics.java
+++ LivePhysics/src/ie/flax/livephysics/client/LivePhysics.java
@@ -1,6 +1,7 @@
 package ie.flax.livephysics.client;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 import org.jbox2d.common.Vec2;
 import org.jbox2d.dynamics.BodyType;
@@ -9,6 +10,7 @@ import org.jbox2d.dynamics.World;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.Random;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -19,6 +21,7 @@ public class LivePhysics implements EntryPoint {
     ArrayList<GameObject> objs = new ArrayList<GameObject>();
     final int FRAMES_PER_SECOND = 35;
     static final int PTM_RATIO = 30;
+    static int x, y, w, h;
 
     @Override
     public void onModuleLoad() {
@@ -45,15 +48,28 @@ public class LivePhysics implements EntryPoint {
         Timer timer = new Timer() {
             @Override
             public void run() {
+                long oldTime = new Date().getTime();
                 world.step(1.0f / FRAMES_PER_SECOND, 10, 10);
                 if (!update()) {
                     cancel();
                 }
                 dm.draw();
+                long newTime = new Date().getTime();
+                System.out.println(newTime - oldTime);
             }
 
         };
         timer.scheduleRepeating(1000 / FRAMES_PER_SECOND);
+
+        Timer t = new Timer() {
+
+            @Override
+            public void run() {
+                objs.add(new Block(Random.nextInt(Window.getClientWidth()), 0,
+                        10, 10, BodyType.DYNAMIC));
+            }
+        };
+        // t.scheduleRepeating(200);
     }
 
     private void setupWalls() {
